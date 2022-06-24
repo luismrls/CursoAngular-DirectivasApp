e1f93af6d8100806b9ef0024c5454cde5e6a8b54
Luis Morales <dev.luismorales@gmail.com>
2022-06-24 12:50:27 -0500

    se creo una nueva directiva 'estructural' para ocultar y mostrar un elemento html

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorMsgDirective } from './directives/error-msg.directive';
+import { CustomIfDirective } from './directives/custom-if.directive';
 
 
 
 @NgModule({
   declarations: [
-    ErrorMsgDirective
+    ErrorMsgDirective,
+    CustomIfDirective
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ErrorMsgDirective
+    ErrorMsgDirective,
+    CustomIfDirective
   ]
 })
 export class SharedModule { }
